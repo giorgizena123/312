794dc9b17a011023406610be35338a177b9d486d
giorgizena123 <zenaishvili621@gmail.com>
2024-10-25 00:58:45 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+ //1)
+ int a=5;
+ int b=7;
+ int c=2;
+ double k=a+b+c;
+        System.out.println(k/3);
+  //2)
+      int x=8;
+      String z= (x%2==1)? "კენტია": "ლუწია";
+        System.out.println(z);
+
+//3)
+        int q=16;
+        int m=11;
+        int max=(m>q) ? m:q;
+        System.out.println(max);
+//4)
+        int p=5178;
+        int Num=p%10+(p/10)%10+(p/100)%10+p/1000;
+        System.out.println(Num);
+ //5)
+
+        int al=30;
+        int be=60;
+        int ga=90;
+        if(al+be+ga==180){
+            System.out.println("სამკუთხედი აიგება");
         }
+        else{
+            System.out.println("ვერ აიგება სამკუთედი");
+        }
+
     }
 }
